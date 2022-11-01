df52340ba3a7ecc4e06f4ad2dc9bbb4c564e1a32
Nunik95 <95641980+Nunik95@users.noreply.github.com>
2022-11-01 17:14:41 +0400

    added thread for exporting data from db Employee

--- src/main/java/com/nuranaibrahimova/spring/springboot/spring_course_springboot/SpringCourseSpringbootApplication.java
+++ src/main/java/com/nuranaibrahimova/spring/springboot/spring_course_springboot/SpringCourseSpringbootApplication.java
@@ -2,25 +2,37 @@ package com.nuranaibrahimova.spring.springboot.spring_course_springboot;
 
 import com.nuranaibrahimova.spring.springboot.spring_course_springboot.controller.MyRESTController;
 import com.nuranaibrahimova.spring.springboot.spring_course_springboot.threads.ExportEmployeesTask;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class SpringCourseSpringbootApplication {
+    @Autowired
+    private ExportEmployeesTask task;
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringCourseSpringbootApplication.class, args);
 
+
+    }
+
+    @Bean
+    public ScheduledExecutorService scheduledExecutorService(){
         ScheduledExecutorService scheduler
                 = Executors.newSingleThreadScheduledExecutor();
         ScheduledExecutorService executorService;
-        executorService = Executors.newSingleThreadScheduledExecutor();
-        ExportEmployeesTask task=new ExportEmployeesTask();
+        Executors.newSingleThreadScheduledExecutor();
         task.start();
-        scheduler.scheduleWithFixedDelay(task, 0, 1, TimeUnit.SECONDS);
+
+        scheduler.scheduleWithFixedDelay(task, 0, 1, TimeUnit.MINUTES);
+        return scheduler;
     }
 }
